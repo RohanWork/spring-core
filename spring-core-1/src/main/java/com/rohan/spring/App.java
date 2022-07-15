4422f4cdf8c086b9442da7fa29719415d8cecffd
Rohan Katare <88019506+RohanWork@users.noreply.github.com>
2022-07-15 23:02:18 +0530

    Executed spring core programme
    Creating the basic spring core programme by using beans and setter injection.

---
+++ spring-core-1/src/main/java/com/rohan/spring/App.java
@@ -0,0 +1,19 @@
+package com.rohan.spring;
+
+import org.springframework.context.ApplicationContext;
+import org.springframework.context.support.ClassPathXmlApplicationContext;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+        ApplicationContext context = new ClassPathXmlApplicationContext("config.xml");
+        student student = (student)context.getBean("student1");
+        System.out.println(student);
+    }
+}
